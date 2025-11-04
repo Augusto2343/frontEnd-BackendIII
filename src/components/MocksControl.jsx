99ea812cfc33411bb1fa5d2b5238d9b99742b377
augustoxd2343@gmail.com <augustoxd2343@gmail.com>
2025-11-03 23:43:29 -0300

    Subiendo el front terminado

--- src/components/MocksControl.jsx
+++ src/components/MocksControl.jsx
@@ -2,40 +2,145 @@ import { useContext, useState } from "react";
 import { MockContext } from "./context/MockingContext";
 
 const MocksControl = () =>{
-    const {mockingUsrs,mockingPets, generateData} = useContext(MockContext);
+    const {mockingUsers,mockingPets, generateData} = useContext(MockContext);
     const [response,setResponse] =useState();
+    const [generateDataForm, setGenerateDataForm] = useState({
+        cantPets:0,
+        cantUsers:0
+    });
+    const [responseUsrs,setResponseUsrs] = useState();
+    const [responsePets,setResponsePets] = useState();
+    const [mockUsrForm, setMockUsrForm] = useState();
+    const [mockPetForm, setMockPetForm] = useState();
+    const handleMockPetsChange = (e) =>{
+        setMockPetForm(e.target.value)
+    }
+    const handleMockUsrsChange = (e) => {
+        setMockUsrForm(e.target.value)
+    }
+    const handleGenerateDataChange = (e,input)=>{
+        let dataGen = generateDataForm
+        switch (input) {
+            case "cantPets":
+                dataGen.cantPets = e.target.value;
+                break;
+            case "cantUsers":
+                dataGen.cantUsers = e.target.value;
+                break;
+            default:
+                break;
+            }
+        setGenerateDataForm(dataGen);
+    }
+    const handleMockPets = async() =>{
+        const request = await mockingPets(mockPetForm);
+        console.log(request);
+        setResponsePets(request);
+        
+    }
+    const handleMockUsers = async() =>{
+        const request = await mockingUsers(mockUsrForm);
+        console.log(request);
+        setResponseUsrs(request);
+        
+    }
+    const handleGenerateData = async() =>{
+        const request =await generateData(generateDataForm.cantPets, generateDataForm.cantUsers);
+        console.log(request);
+        setResponsePets(request.pets);
+        setResponseUsrs(request.users);
+
+    }
     return(
         <>
-            <section id="mocking" className="">
-                    <form onSubmit={(e) =>{
+            <section id="mocking" className="flex flex-row items-center justify-center w-screen gap-4">
+            <form 
+                    
+                    onSubmit={(e) =>{
                     e.preventDefault();
-                    if(postFormData.role == "") return Swal.fire({icon:"error",title:"Seleccione un rol"})
-                    handleSubmitPost()
-                    }} className="max-w-300 min-w-60 h-full flex flex-col items-center justify-center">
+                    if(mockPetForm == null || 0) return Swal.fire({icon:"error",title:"Seleccione un rol"})
+                    handleMockPets()
+                    }} 
+                    className="max-w-300 min-w-60 h-full flex flex-col items-center justify-center">
                     <div className="w-full p-2 border border-gray-300 rounded-md">
                     <div className="d-flex flex-col items-center justify-center">
-                        <label className="W-full text-center" htmlFor="nombre">Nombre de la mascota</label>
-                        <input onChange={(e) => handlePostChange(e,e.target.id)} id="nombre" className="w-full p-2 border border-gray-300 rounded-md border-gray-300 bg-gray-900" name="nombre" type="text" required/>
+                        <label className="W-full text-center" htmlFor="cant">Cantidad de mocks de mascotas</label>
+                        <input onChange={(e) => handleMockPetsChange(e)} id="cant" className="w-full p-2 border border-gray-300 rounded-md border-gray-300 bg-gray-900" name="cant" type="number" required/>
                     </div>
                     </div>
                     
-                    <button className="w-full p-2 bg-blue-500 text-white rounded-md" type="submit">Postear mascota</button>
+                    <button className="w-full p-2 bg-blue-500 text-white rounded-md" type="submit">Generar mock de mascotas</button>
+                </form>
+                <form
+                    
+                    onSubmit={(e) =>{
+                    e.preventDefault();
+                    if(mockUsrForm == 0 || null) return Swal.fire({icon:"error",title:"Seleccione un rol"})
+                    handleMockUsers()
+                    }} 
+                    className="max-w-300 min-w-60 h-full flex flex-col items-center justify-center">
+                    <div className="w-full p-2 border border-gray-300 rounded-md">
+                    <div className="d-flex flex-col items-center justify-center">
+                        <label className="W-full text-center" htmlFor="cant">Cantidad de mocks de usuarios</label>
+                        <input onChange={(e) => handleMockUsrsChange(e)} id="cant" className="w-full p-2 border border-gray-300 rounded-md border-gray-300 bg-gray-900" name="cant" type="number" required/>
+                    </div>
+                    </div>
+                    
+                    <button className="w-full p-2 bg-blue-500 text-white rounded-md" type="submit">Generar mock de Usuarios</button>
+                </form>
+                <form 
+                    onSubmit={(e) =>{
+                    e.preventDefault();
+                    if(generateDataForm.cantPets == 0 || generateDataForm.cantUsers == 0) return Swal.fire({icon:"error",title:"Seleccione un rol"})
+                    handleGenerateData()
+                    }} className="max-w-300 min-w-60 h-full flex flex-col items-center justify-center">
+                    
+                    <div className="w-full p-2 border border-gray-300 rounded-md">
+                    <div className="d-flex flex-col items-center justify-center">
+                        <label className="W-full text-center" htmlFor="cantPets">Cantidad de mascotas</label>
+                        <input onChange={(e) => handleGenerateDataChange(e,e.target.id)} id="cantUsers" className="w-full p-2 border border-gray-300 rounded-md border-gray-300 bg-gray-900" name="cant" type="number" required/>
+                    </div>
+                    <div className="d-flex flex-col items-center justify-center">
+                        <label className="W-full text-center" htmlFor="cantUsers">Cantidad de usuarios</label>
+                        <input onChange={(e) => handleGenerateDataChange(e,e.target.id)} id="cantPets" className="w-full p-2 border border-gray-300 rounded-md border-gray-300 bg-gray-900" name="cant" type="number" required/>
+                    </div>
+                    </div>
+                    
+                    <button className="w-full p-2 bg-blue-500 text-white rounded-md" type="submit">Generar mock de mascotas</button>
                 </form>
             </section>
             <section id="resultMocking">
+                <h2 className="text-gray-200 text-lg">Mascotas creadas</h2>
                 {
-                    response?.map((item, index) =>(
+                    responsePets?.map((item, index) =>(
                         <>
                         <div key={index} className="bg-zinc-700 flex flex-col items-center justify-center">
                                 
-                                <h3 className="text-gray-200 text-xl" >{item.name? item.name: item.first_name }</h3>
-                                <h4 className="text-gray-300 text-lg">{item._id}</h4>
-                                <h4 className="text-gray-200 text-xl">{item.email? item.email: item.adopted}</h4>
-                                <h4 className="text-gray-200 text-xl">{item.role? item.role : item.adopted? item.owner: ""}</h4>
+                                <h3 className="text-gray-200 text-xl" >Nombre: {item.name }</h3>
+                                <h4 className="text-gray-300 text-lg">ID: {item._id}</h4>
+                                <h4 className="text-gray-200 text-xl">Adoptado: {item.adopted.toString()}</h4>
+                                <h4 className="text-gray-200 text-xl">Dueño: {item.adopted? item.owner :  "No tiene"}</h4>
                         </div>
                         </>
+                
                     )
-                )}
+                    )
+                    
+                }
+                <h2 className="text-gray-200 text-lg">Usuarios creados </h2>
+                {
+                    responseUsrs?.map((item,index) =>(
+                        <>
+                        <div key={index} className="bg-zinc-700 flex flex-col items-center justify-center">
+                                
+                                <h3 className="text-gray-200 text-xl" >N{ item.first_name }</h3>
+                                <h4 className="text-gray-300 text-lg">{item._id}</h4>
+                                <h4 className="text-gray-200 text-xl">{item.email}</h4>
+                                <h4 className="text-gray-200 text-xl">{ item.role}</h4>
+                        </div>
+                        </>
+                    ))
+                }
             </section>
         </>
     )
--- src/components/context/MockingContext.jsx
+++ src/components/context/MockingContext.jsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
+import { createContext } from "react";
 import Swal from "sweetalert2";
 
-export const MockContext = useContext();
+export const MockContext = createContext();
 
 const MockContextProvider = ({children}) =>{
     const mockingPets = async(cant) =>{
         try {
-            const response = await fetch(`${import.meta.env.VITE_BACK_URL}/api/mocks/mockingPets/${cant}`);
+            const response = await fetch(`${import.meta.env.VITE_URL_BACK}/api/mocks/mockingPets/${cant}`);
             if(!response.ok) return Swal.fire({icon:"error",title:"Error al hacer mocking de mascotas"});
             const responseBod = await response.json();
             Swal.fire({
@@ -20,7 +20,7 @@ const MockContextProvider = ({children}) =>{
     }   
     const mockingUsers = async(cant) =>{
         try {
-            const response = await fetch(`${import.meta.env.VITE_BACK_URL}/api/mocks/mockingUsers/${cant}`);
+            const response = await fetch(`${import.meta.env.VITE_URL_BACK}/api/mocks/mockingUsers/${cant}`);
             if(!response.ok) return Swal.fire({icon:"error",title:"Error al hacer mocking de usuarios"});
             const responseBod = await response.json();
             Swal.fire({
@@ -32,17 +32,23 @@ const MockContextProvider = ({children}) =>{
             return Swal.fire({icon:"error",title:"Error",text:{error}})
         }
     }
-    const generateData = async(cantPets,cantUsrs) =>{
+    const generateData = async(cantPets,cantUsers) =>{
         try {
-            const response = await fetch(`${import.meta.env.VITE_BACK_URL}/api/mocks/genrateData/${cantPets}/${cantUsrs}`);
+            const response = await fetch(`${import.meta.env.VITE_URL_BACK}/api/mocks/generateData/${cantPets}/${cantUsers}`);
+            console.log(response.body);
+            
             if(!response.ok) return Swal.fire({icon:"error",title:"Error al crear el mocking data"});
             const responseBod = await response.json();
+            console.log(responseBod);
+            
             Swal.fire({
                 icon:"success",
                 title:"Mocking creado"
             })
             return responseBod;
         } catch (error) {
+            console.log(error);
+            
             return Swal.fire({icon:"error",title:"Error",text:{error}})
         }
     }
